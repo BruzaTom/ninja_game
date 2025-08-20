7ba042ca0cb5f43c0d2ceeaddf9028a8ec1357d4
tomux <thomasagomez77@gmail.com>
2025-08-19 22:10:50 -0500

    images, input and collisions

--- ninja_game.py
+++ ninja_game.py
@@ -3,20 +3,53 @@ import pygame
 
 class Game:
     def __init__(self):
-
         pygame.init()
 
         pygame.display.set_caption('ninja game')
         self.screen = pygame.display.set_mode((640, 480))
+        
         self.clock = pygame.time.Clock()
+        
+        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+        self.img.set_colorkey((0, 0, 0))
+        
+        self.img_pos = [160, 260]
+        #x and y bool values
+        self.movement = [False, False]
+        #rect for collision testing
+        self.collision_area = pygame.Rect(50, 50, 300, 50)
 
     def run(self):
         while True:
+            self.screen.fill((14, 219, 248))
+            #rect that is the img demensions and position
+            img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
+                #pygame.Rect(*self.img_pos, *self.img.get_size())
+            #draw rect in diffrent colors if collided with
+            if img_r.colliderect(self.collision_area):
+                pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
+            else:
+                pygame.draw.rect(self.screen, (0, 50, 155), self.collision_area)
+            #img y movement
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
+            #draw img
+            self.screen.blit(self.img, self.img_pos)
+            #input
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False
+            #finishers
             pygame.display.update()
             self.clock.tick(60)
 
